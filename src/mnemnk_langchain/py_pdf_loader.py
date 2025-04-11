def425a89bb4e58b8232d5a06c5cc48cd441e2d6
Akira Ishino <akira@lumilab.jp>
2025-04-11 17:11:50 +0900

    feat: 🎸 PyPDFLoader agent

---
+++ src/mnemnk_langchain/py_pdf_loader.py
@@ -0,0 +1,24 @@
+from . import BaseAgent, run_agent
+
+
+class PyPDFLoaderAgent(BaseAgent):
+    """Load PDF file using PyPDFLoader."""
+
+    def process_input(self, _ch: str, _kind: str, value: any):
+        from langchain_community.document_loaders import PyPDFLoader
+
+        loader = PyPDFLoader(value)
+        for page in loader.lazy_load():
+            doc_dict = {
+                "metadata": page.metadata,
+                "page_content": page.page_content,
+            }
+            self.write_out("document", "document", doc_dict)
+            self.write_out("content", "text", page.page_content)
+
+def main():
+    run_agent(PyPDFLoaderAgent)
+
+
+if __name__ == "__main__":
+    main()
