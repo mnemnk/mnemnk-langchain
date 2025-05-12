a4561370ab5e3348777b940963312ee1799a7a13
Akira Ishino <akira@lumilab.jp>
2025-05-12 11:22:57 +0900

    feat: 🎸 add language and translation config to YoutubeLoader

--- src/mnemnk_langchain/youtube_loader.py
+++ src/mnemnk_langchain/youtube_loader.py
@@ -13,7 +13,18 @@ class YoutubeLoaderAgent(BaseAgent):
         from langchain_community.document_loaders import YoutubeLoader
 
         if data.value.startswith("https://www.youtube.com/"):
-            loader = YoutubeLoader.from_youtube_url(data.value, add_video_info=False)
+            language = self.config.get("language", "en").split(",")
+            translation = self.config.get("translation")
+            if translation == "":
+                translation = None
+
+            loader = YoutubeLoader.from_youtube_url(
+                data.value,
+                add_video_info=False,
+                language=language,
+                translation=translation,
+            )
+
             documents = loader.load()
             if not documents:
                 logger.error("No documents found")
